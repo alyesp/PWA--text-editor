5df8fcd99c2a24fe4a8a4a4347d34449f8121188
Alyssa Esparza <alyssa.esparza95@gmail.com>
2022-11-15 17:19:08 -0800

    updated README, install.js, database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,11 +11,32 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+// Accepted content and adds it to the database (IndexDB)
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1 );
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  console.log('putDb implemented');
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const results = await request;
+  result
+
+  ? console.log('Data retrieved from database')
+  : console.log('Data not found in Database');
+
+  return results?.value;
+
+};
 
 initdb();
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Text Editor Starter Code
+# PWA - Text Editor 'JATE' (Just another text editor)
+
+### Table of Contents
+- [Description](#description)
+- [Usage](#usage)
+- [Heroku Link](#heroku-link)
+- [Images](images)
+- [Technologies Used](#technologies-used)
+- [GitHub Link](#github-link)
+
+## Description
+J.A.T.E was created as a simple text editor app that can function both online and offline, with a variety of data persistence options in order to ensure that data is not lost if the server is offline. The application first looks to use the data in the indexedDB to populate the editor, that id it cannot access that it will use the local storage. For use offline, this application can be downloaded to your desktop or laptop as an application. 
+
+## Usage 
+To use this app to fo the link provided below. For the offline usage, go to the link and hit the "install" button to install the app locally for offline use.
+
+## Heroku Link:
+
+## Images:
+
+## Technologies Used:
+
+### GitHub Link:
+
+## 
